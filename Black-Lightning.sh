69348c183cb375ed4982aa946bc6e4d6a6912224
Tester9958 <103098781+Tester9958@users.noreply.github.com>
2022-04-08 14:15:19 +0530

    Update Black-Lightning.sh

--- Black-Lightning.sh
+++ Black-Lightning.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/KeinShin/Black-Lightning /root/KeinShin
 cp PR/.env /root/KeinShin/.env
 cd /root/KeinShin
-pip install -U -r requirements.txt
+pip install -r requirements.txt
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t Black Lightning
 docker run --privileged --env-file .env --rm -i Black Lightning
 
