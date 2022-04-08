39f18cf45de86ba29158e21eed00342ce28e70dc
Tester9958 <103098781+Tester9958@users.noreply.github.com>
2022-04-08 13:35:36 +0530

    Update Black-Lightning.sh

--- Black-Lightning.sh
+++ Black-Lightning.sh
@@ -3,8 +3,8 @@ cp PR/.env /root/KeinShin/.env
 cd /root/TeamUltroid
 pip install -U -r requirements.txt
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-
-
-
-
 docker run --privileged --env-file .env --rm -i ultroid
+
+
+
+
