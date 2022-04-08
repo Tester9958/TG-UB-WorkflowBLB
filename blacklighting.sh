4b0be2475df77a0dd58f8307509a6ca092b03b1c
Tester9958 <103098781+Tester9958@users.noreply.github.com>
2022-04-08 17:44:42 +0530

    Update blacklighting.sh

--- blacklighting.sh
+++ blacklighting.sh
@@ -1,1 +1,5 @@
 git clone https://github.com/KeinShin/Black-Lightning
+cp CREDS/local_config.py /root/keinshin/local_config.py
+cd/root/keinshin 
+pip install -r requirements.txt 
+python3 -m Black-Lightning
